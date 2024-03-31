1fdf1fc0ed900675e620293aff2755b878e40ab2
Your Name <you@example.com>
2024-03-30 21:50:07 -0700

    Add completed extra credit tasks

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ function makeGrid(size) {
   grid.replaceChildren();
   if (size > 100) size = 100;
   if (!Number.isInteger(size) || Number.isInteger(size) && size < 0) size = 16;
+  gridSize = size;
 
   for (let row = 0; row < size; row++) {
     const rowElement = document.createElement("div");
@@ -10,7 +11,7 @@ function makeGrid(size) {
       const itemElement = document.createElement("div");
       itemElement.setAttribute("class", "item");
       itemElement.setAttribute("id", `r${row}i${item}`);
-      itemElement.setAttribute("data-shade", "0");
+      itemElement.setAttribute("data-shade", 0);
 
       itemElement.addEventListener("mouseenter", handleClick);
 
@@ -21,12 +22,22 @@ function makeGrid(size) {
   }
 }
 
+function randomColor(num){
+  return Math.floor(Math.random() * (num + 1))
+}
+
 function handleClick(event) {
   switch(gameType) {
-    case 0:
+    case 1:
+      event.target.style["background-color"] = 
+        `rgb(${randomColor(255)},${randomColor(255)},${randomColor(255)})`;
+      break;
+    case 2:
+      incrementShade(event.target);
+      break;
+    default:
       event.target.classList.add("shade-9");
       break;
-    
   }
 }
 
@@ -36,10 +47,36 @@ const reset = document.querySelector("#reset");
 reset.addEventListener("click", () => makeGrid(getNewDimensions()));
 
 let gameType = 0;
+let gridSize = 16;
 
 makeGrid(16);
 
 function getNewDimensions() {
   let num = parseInt(prompt("Enter dimension", 16));
   return Number.isInteger(num) && num > 0 ? num : 16;
+}
+
+function incrementShade(item) {
+  let shade = parseInt(item.getAttribute("data-shade"));
+  if( shade >= 9 ) return;
+  console.log(shade);
+  item.classList.remove(`shade-${parseInt(shade)}`)
+  item.classList.add(`shade-${parseInt(shade+1)}`);
+  ++shade;
+  item.setAttribute("data-shade",parseInt(shade));
+}
+
+function setNormal() {
+  gameType = 0;
+  makeGrid(gridSize);
+}
+
+function setRGB() {
+  gameType = 1;
+  makeGrid(gridSize);
+}
+
+function setShades() {
+  gameType = 2;
+  makeGrid(gridSize);
 }
